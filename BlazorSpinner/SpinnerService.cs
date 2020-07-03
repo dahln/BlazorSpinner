398d6742f0e49ae1329bc1e118f93989e89d7225
dahln <dahln@outlook.com>
2020-07-02 18:58:56 -0600

    Add project files.

---
+++ BlazorSpinner/SpinnerService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorSpinner
+{
+	public class SpinnerService
+	{
+		public event Action OnShow;
+		public event Action OnHide;
+
+		public void Show()
+		{
+			OnShow?.Invoke();
+		}
+
+		public void Hide()
+		{
+			OnHide?.Invoke();
+		}
+	}
+}
+
